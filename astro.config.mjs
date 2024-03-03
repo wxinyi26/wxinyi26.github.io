b44a0b86db4e8bc46531008653a92fc58d4f0e84
XinyiWang <xinyiwg02@gmail.com>
2024-03-02 19:41:41 -0500

    done1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@
 
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://XinyiWg.github.io",
   base: "",
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon()],
 });
