9d6f7366858499e09b4e8a62d7d922ab09db5c1c
Xinyi Wang <xinyiwang11@hotmail.com>
2025-09-05 13:44:22 +0800

    a lot of updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
-  site: "https://XinyiWg.github.io",
+  site: "https://wxinyi26.github.io",
   base: "",
   integrations: [tailwind(), icon()],
 });
