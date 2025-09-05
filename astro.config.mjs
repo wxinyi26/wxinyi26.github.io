5de167372bd8ccc2837679c1b09379937fca8bbe
Xinyi Wang <xinyiwang11@hotmail.com>
2025-09-05 14:02:25 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://wxinyi26.github.io",
   base: "",
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), sitemap()],
 });
